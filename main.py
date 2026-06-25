2005ba4d87c7855b1517d1161c88de7b774307db
Lewis Edmond <lewismedmond@gmail.com>
2026-06-25 15:50:27 +0100

    filtered out all pokemon forms from main list

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-pokemon = pd.DataFrame(columns=["Name"])
+pokemon = pd.DataFrame(columns=["Name", "Names_with_forms"])
 
 req = requests.get("https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number")
+
+#https://bulbapedia.bulbagarden.net/wiki/Charmander_(Pok%C3%A9mon)
 
 content = req.content
 soup = BeautifulSoup(content, "html.parser")
@@ -15,18 +17,30 @@ print(len(generations))
 names = []
 links = []
 
+rows = soup.select('table[class*="roundy"] tr[style*="background:#FFF"]')
+
+for row in rows:
+
+    if row.find("td").get("rowspan") != None:
+        names.append(row.find_all("td", style = None)[1].text.strip())
+
+
+names_with_forms = []
+
 for generation in generations:
-    names.extend([item.text for item in generation.find_all("td", style = None)[1::2]])  # This will print the names of the Pokémon in the first generation
-    links.extend([item.find("a")["href"] for item in generation.find_all("td", style = None)[1::2]])  # This will print the links of the Pokémon in the first generation
+    names_with_forms.extend([name.text for name in generation.find_all("td", style = None)[1::2]])
 
 
-for name, link in zip(names, links):
-    pokemon = pd.concat([pokemon, pd.DataFrame({"Name": [name]})], ignore_index=True)
+pokemon_with_forms = pd.concat([pokemon, pd.DataFrame({"Names_with_forms": names_with_forms})], ignore_index = True)
+pokemon = pd.concat([pokemon, pd.DataFrame({"Name":names})], ignore_index = True)
+
 
 print(pokemon)
+print(pokemon_with_forms)
+
 
 
 # -- June 22nd, 2026
 # add pokemon to the df one at a time, 
 # identify extra forms by checking for a td with a rowspan attribute
-# and then probably handle them individually by checking their respective webpages; some forms are competitively distinct
+# and then probably handle them individually by checking their respective webpages; some forms are competitively distinct(variable) name: Any
