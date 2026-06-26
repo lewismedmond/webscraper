e33cefcbb5ddca77ba79f99fa8177328e33d9d5b
Lewis Edmond <lewismedmond@gmail.com>
2026-06-26 14:34:12 +0100

    scrapes pokemon's typing, abilities and hidden abilities separately and base stats. can distinguish megas but no other forms

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+from scraper import scrape_pokemon
 
-pokemon = pd.DataFrame(columns=["Name", "Names_with_forms"])
+pokemon = pd.DataFrame(columns=["Name", "Links"])
 
 req = requests.get("https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number")
 
@@ -12,34 +13,32 @@ content = req.content
 soup = BeautifulSoup(content, "html.parser")
 generations = soup.find_all("table", class_="roundy")
 
-print(len(generations))
 
 names = []
 links = []
+extra_info = pd.DataFrame(columns = ["Type1", "Type2"]) #, "HP", "ATK", "DEF", "SPATK", "SPDEF", "SPEED", "BST"])
 
 rows = soup.select('table[class*="roundy"] tr[style*="background:#FFF"]')
 
 for row in rows:
   if row.find("td").get("rowspan") != None:
     names.append(row.find_all("td", style = None)[1].text.strip())
+    links.append(row.find("a")["href"])
+    extra_info.loc[len(extra_info)+1] = scrape_pokemon(links[-1])
 
 
-names_with_forms = []
+#names_with_forms = []
 
-for generation in generations:
-  names_with_forms.extend([name.text for name in generation.find_all("td", style = None)[1::2]])
+#for generation in generations:
+#  names_with_forms.extend([name.text for name in generation.find_all("td", style = None)[1::2]]) 
 
 
 #pokemon_with_forms = pd.concat([pokemon, pd.DataFrame({"Names_with_forms": names_with_forms})], ignore_index = True)
-pokemon = pd.concat([pokemon, pd.DataFrame({"Name":names})], ignore_index = True)
+pokemon = pd.concat([pokemon, pd.DataFrame({"Name":names, "Links":links})], ignore_index = True)
 
 
 print(pokemon)
+print(extra_info)
 #print(pokemon_with_forms)
 
 
-
-# -- June 22nd, 2026
-# add pokemon to the df one at a time, 
-# identify extra forms by checking for a td with a rowspan attribute
-# and then probably handle them individually by checking their respective webpages; some forms are competitively distinct(variable) name: Any
--- scraper.py
+++ scraper.py
@@ -5,20 +5,54 @@ import re
 
 
 def scrape_pokemon(pokemon):
-  page_req = requests.get("https://bulbapedia.bulbagarden.net/wiki/" + pokemon + "_(Pok%C3%A9mon)")
-  page = BeautifulSoup(page_req.content, "html.parser")
-  summary = page.select('table[class*="roundy infobox"] tr td[style*="font-size: 85%"]')
+  prefix = "https://bulbapedia.bulbagarden.net/"
+  page_req = requests.get(prefix + pokemon) # Finding the right webpage
+
+  page = BeautifulSoup(page_req.content, "html.parser")  # Parsing html
+  
+  
+
+  
+
+  type_links = page.select('table[class*="roundy infobox"] a[title*="(type)"]') # Selecting hyperlinks from the top right summary that redirect to a type page
+
+
+  ability_links = page.select('table[class*="roundy infobox"] a[title*="(Ability)"]') # Selecting hyperlinks from the top right summary that redirect to an ability page
+
+ 
 
   pokemon_types = []
-  #print(summary)
-  for item in summary:
-    if re.search("type", item.find("a")["href"]) and not re.search("Unknown", item.find('b').text.strip()):
-      pokemon_types.append(item.find('b').text.strip())
+  pokemon_abilities = [[],[]]
   
-  hp_link = page.find("a", title = "HP")
-  row = hp_link.find_parent("tr")
-  base_hp = row.find_all("div")[1].text.strip()
-  print(base_hp)
-  return(pokemon_types)
+  for item in type_links:
+    if item.text.strip() != "Unknown" and not re.search(pokemon.split('_')[0][5::], item.find_parent("td").text):
+      pokemon_types.append(item.text.strip())  # Scraping the pokemon's types from the hyperlinks. For some reason there are invisible "Unknown" types
 
-print(scrape_pokemon("Bulbasaur"))
+
+  for item in ability_links:
+
+    if item.text.strip() != "Cacophony":
+      if re.search( "Hidden Ability", item.find_parent("td").text):
+        pokemon_abilities[1].append(item.text.strip())
+      elif not re.search("Mega " + pokemon.split('_')[0][5::], item.find_parent("td").text):
+        pokemon_abilities[0].append(item.text.strip())
+        # Scraping the pokemon's abilities from the hyperlinks. For some reason there are invisible "Cacophony" abilities
+
+  if(len(pokemon_types) == 1):
+    pokemon_types.append("None")
+  
+  base_stats = []
+
+  hp_link = page.find("a", title = "HP")  # Finding the HP stat hyperlink
+  stat_table = hp_link.find_parent("tbody").find_all("th")[4::] # Scraping each stat and the total
+  
+
+  for stat in stat_table:
+    base_stats.append(stat.find_all("div")[1].text.strip()) # Selecting the actual number
+  
+  
+
+  return(pokemon_types + pokemon_abilities + base_stats)
+
+
+print(scrape_pokemon("wiki/Squawkabilly_(Pok%C3%A9mon)"))
